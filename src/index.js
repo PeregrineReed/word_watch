65b5ae8f46b12d36fae29b02f0fc801d69b00cd3
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-23 10:10:38 -0600

    Added display for word being added

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ $(document).ready(() => {
   $(".break-down").on("click", async function() {
     const tag = $('.words');
     const input = tag[0].value
-    let x = await fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+    let response = await fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ word: { value: input } })
     });
-    x = await x.json()
-    console.log(x)
+    let results = await response.json();
+    $('.submit-msg').text(results.message);
+    setTimeout(function() {
+        $('.submit-msg').text('')
+    }, 1000);
   })
 })
