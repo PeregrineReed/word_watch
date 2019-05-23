c1f97202ee209ce2859591592fd86592bff12954
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-05-23 09:46:19 -0600

    Adds POST to word watch on input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $(".break-down").on("click", async function() {
+    const tag = $('.words');
+    const input = tag[0].value
+    let x = await fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ word: { value: input } })
+    });
+    x = await x.json()
+    console.log(x)
+  })
 })
